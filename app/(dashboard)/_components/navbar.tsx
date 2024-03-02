c97d6bff4d65a97e860d943649048a9612897b23
Laz <lazgerasimov@gmail.com>
2024-03-02 21:30:15 +0200

    create navbar scaffold

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center gap-x-4 p-5'>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
