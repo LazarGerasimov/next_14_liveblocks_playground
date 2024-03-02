8b4e364f3c6cb34b1b5533438ceba375f73c7b28
Laz <lazgerasimov@gmail.com>
2024-03-02 21:34:13 +0200

    add navbar user button

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import React from 'react'
+import { UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
   return (
     <div className='flex items-center gap-x-4 p-5'>
-      Navbar
+      <div className='hidden lg:flex lg:flex-1'>
+        {/* Search */}
+      </div>
+      <UserButton />
     </div>
   )
 }
