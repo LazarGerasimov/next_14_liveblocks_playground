ef88230c0e83b25a765a4dfc5beba3e116dd9478
Laz <lazgerasimov@gmail.com>
2024-03-08 19:05:02 +0200

    upd org switcher styles

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -40,6 +40,14 @@ const OrgSidebar = () => {
               justifyContent: "center",
               alignItems: "center",
               width: "100%"
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundColor: "white"
             }
           }
         }}
