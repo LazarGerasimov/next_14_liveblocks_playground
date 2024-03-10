7b3e63d0418cd16377d67580d363b773fce3f15f
Laz <lazgerasimov@gmail.com>
2024-03-10 20:39:18 +0200

    add view for users without selected/created org

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,10 @@
-
-
-
-
+import { EmptyOrg } from "./_components/empty-org"
 
 const DashboardPage = () => {
   return (
-    <div>Dashboard</div>
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      <EmptyOrg />
+    </div>
   )
 }
 
