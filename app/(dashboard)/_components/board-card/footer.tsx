d6d0f705e975b1e6d54fec900102e9d56d666dad
Laz <lazgerasimov@gmail.com>
2024-03-22 21:39:00 +0200

    update board card styles

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -20,8 +20,11 @@ const Footer = ({
 }: FooterProps) => {
   return (
     <div className="relative bg-white p-3">
-      <p className="text-[13px] truncate">
+      <p className="text-[13px] truncate max-w-[calc(100%-20px)]">
         {title}
+      </p>
+      <p className="opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate">
+        {authorLabel}, {createdAtLabel}
       </p>
     </div>
   )
