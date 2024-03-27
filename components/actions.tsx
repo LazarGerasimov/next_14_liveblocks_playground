0a2ec37d8543ed066aae42ad5f3656d25e8986df
Laz <lazgerasimov@gmail.com>
2024-03-27 15:37:40 +0200

    add delete board confirmation modal

--- components/actions.tsx
+++ components/actions.tsx
@@ -13,6 +13,8 @@ import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { api } from "@/convex/_generated/api";
+import ConfirmModal from "./confirm-modal";
+import { Button } from "./ui/button";
 
 interface ActionsProps {
   children: React.ReactNode;
@@ -64,13 +66,20 @@ export const Actions = ({
           <Link2 className="h-4 w-4 mr-2" />
           Copy board link
         </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={onDelete}
-          className="p-3 cursor-pointer"
+        <ConfirmModal
+          header="Delete board?"
+          description="This will delete the board and its whole content"
+          disabled={pending}
+          onConfirm={onDelete}
         >
-          <Trash2 className="h-4 w-4 mr-2" />
-          Delete
-        </DropdownMenuItem>
+          <Button
+            variant={"ghost"}
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+          >
+            <Trash2 className="h-4 w-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
       </DropdownMenuContent>
     </DropdownMenu>
   )
