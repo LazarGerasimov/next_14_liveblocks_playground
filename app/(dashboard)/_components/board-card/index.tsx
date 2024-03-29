af08766d41b5a850417749389102ae1572da0acf
Laz <lazgerasimov@gmail.com>
2024-03-29 22:19:41 +0200

    implement add board to favourites functionality

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -10,6 +10,9 @@ import Footer from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Actions } from "@/components/actions";
 import { MoreHorizontal } from "lucide-react";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 interface BoardCardProps {
   id: string;
@@ -40,6 +43,26 @@ export const BoardCard = ({
     addSuffix: true
   });
 
+  const {
+    mutate: onFavourite,
+    pending: pendingFavourite
+  } = useApiMutation(api.board.favourite);
+
+  const {
+    mutate: onUnfavourite,
+    pending: pendingUnfavourite
+  } = useApiMutation(api.board.unfavourite);
+
+  const toggleFavourite = () => {
+    if (isFavourite) {
+      onUnfavourite({ id })
+        .catch(() => toast.error("Failed to unfavourite"))
+    } else {
+      onFavourite({ id, orgId })
+        .catch(() => toast.error("Failed to favourite"))
+    }
+  }
+
   return (
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
@@ -66,8 +89,8 @@ export const BoardCard = ({
           title={title}
           authorLabel={authorLabel}
           createdAtLabel={createdAtLabel}
-          onClick={() => { }}
-          disabled={false}
+          onClick={toggleFavourite}
+          disabled={pendingFavourite || pendingUnfavourite}
         />
       </div>
     </Link>
