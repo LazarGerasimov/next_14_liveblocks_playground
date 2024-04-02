d80fe96c0d268c65d6efafbe8b800341c4232e33
Laz <lazgerasimov@gmail.com>
2024-04-02 18:33:45 +0300

    update canvas scaffold

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Info from "./info";
+import Participants from "./participants";
+import Toolbar from "./toolbar";
 
 
 const Canvas = () => {
@@ -9,6 +11,8 @@ const Canvas = () => {
       className="h-full w-full relative bg-neutral-100 touch-none"
     >
       <Info />
+      <Participants />
+      <Toolbar />
     </main>
   )
 }
