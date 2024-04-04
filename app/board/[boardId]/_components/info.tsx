4df8612ae91d365170c8396a6910b4edaf881194
Laz <lazgerasimov@gmail.com>
2024-04-04 18:43:18 +0300

    create info comp skeleton loader

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 const Info = () => {
@@ -8,4 +9,14 @@ const Info = () => {
   )
 }
 
-export default Info
+
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div
+    className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]">
+      <Skeleton className="h-full w-full bg-muted-400" />
+    </div>
+  )
+}
+
+export default Info;
