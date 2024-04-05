61e077c57211ff0bffa05be8bd2d9fe4528412d6
Laz <lazgerasimov@gmail.com>
2024-04-05 15:28:48 +0300

    fix info, participants and toolbar loading skeletons

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from "@/components/ui/skeleton";
+"use client";
 
 
 const Info = () => {
@@ -10,7 +10,7 @@ const Info = () => {
 }
 
 
-Info.Skeleton = function InfoSkeleton() {
+export const InfoSkeleton = () => {
   return (
     <div
       className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]" />
