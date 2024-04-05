930c8519787c99c5df2f4f2b56838cdddffe67ed
Laz <lazgerasimov@gmail.com>
2024-04-05 16:46:51 +0300

    update info comp (add logo button and redirection link)

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,8 +4,6 @@ import Info from "./info";
 import Participants from "./participants";
 import Toolbar from "./toolbar";
 
-import { useSelf } from "@/liveblocks.config";
-
 interface CanvasProps {
   boardId: string;
 }
@@ -14,13 +12,13 @@ const Canvas = ({
   boardId
 }: CanvasProps) => {
 
-  const info = useSelf((me) => me.info);
+
 
   return (
     <main
       className="h-full w-full relative bg-neutral-100 touch-none"
     >
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <Toolbar />
     </main>
