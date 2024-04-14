5dbd1507aaed9ab6f3215644d17a3f6978f414eb
Laz <lazgerasimov@gmail.com>
2024-04-14 20:38:41 +0300

    create cursors presence comp scaffold

---
+++ app/board/[boardId]/_components/cursors-presence.tsx
@@ -0,0 +1,22 @@
+
+"use client";
+
+import { memo } from 'react';
+
+
+import React from 'react'
+
+const CursorsPresence = memo(() => {
+  return (
+    <>
+      <p>
+        Cursors
+      </p>
+    </>
+  )
+}
+)
+
+CursorsPresence.displayName = "";
+
+export default CursorsPresence;
