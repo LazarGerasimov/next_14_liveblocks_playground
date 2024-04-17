f7ea3024dd3aa54364167f17e85fdc1f9a9e2b6a
Laz <lazgerasimov@gmail.com>
2024-04-17 17:34:55 +0300

    update rectangle comp scaff

--- app/board/[boardId]/_components/rectangle.tsx
+++ app/board/[boardId]/_components/rectangle.tsx
@@ -15,8 +15,24 @@ const Rectangle = ({
   onPointerDown,
   selectionColor
 }: RectangleProps) => {
+
+  const { x, y, width, height, fill } = layer;
+
   return (
-    <div>Rectangle</div>
+    <rect
+      className="drop-shadow-md"
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        transform: `translate(${x}px, ${y}px)`
+      }}
+      x={0}
+      y={0}
+      width={width}
+      height={height}
+      strokeWidth={1}
+      fill={"#000"}
+      stroke="transparent"
+    />
   )
 }
 
