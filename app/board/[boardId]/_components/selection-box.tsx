a7a9cf3803ddca166245d699c473045ee9452444
Laz <lazgerasimov@gmail.com>
2024-04-19 19:50:32 +0300

    implement self box selection

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import { useSelf, useStorage } from "@/liveblocks.config";
 import { LayerType, Side, XYWH } from "@/types/canvas";
 import { memo } from "react";
@@ -23,12 +24,25 @@ const SelectionBox = memo(({
   const isShowingHandles = useStorage((root) =>
     soleLayerId && root.layers.get(soleLayerId)?.type !== LayerType.Path);
 
-  
+  const bounds = useSelectionBounds();
+
+  if (!bounds) {
+    return null;
+  }
 
   return (
-    <div>
-
-    </div>
+    <>
+      <rect
+        className="fill-transparent stroke-blue-500 stroke-1 pointer-events-none"
+        style={{
+          transform: `translate(${bounds.x}px, ${bounds.y}px)`,
+        }}
+        x={0}
+        y={0}
+        width={bounds.width}
+        height={bounds.height}
+      />
+    </>
   )
 }
 )
