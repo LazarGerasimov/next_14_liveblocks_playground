9c05d75b14337dca701d6e9dba9f2e04573a0345
Laz <lazgerasimov@gmail.com>
2024-04-21 16:15:24 +0300

    add top right resizing box on clicked rectangles

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -74,6 +74,21 @@ const SelectionBox = memo(({
               // Add resize handler
             }}
           />
+          <rect
+            className="fill-white stroke-1 stroke-blue-500"
+            x={0}
+            y={0}
+            style={{
+              cursor: "nesw-resize",
+              width: `${HANDLE_WIDTH}px`,
+              height: `${HANDLE_WIDTH}px`,
+              transform: `translate(${bounds.x - HANDLE_WIDTH / 2 + bounds.width}px, ${bounds.y - HANDLE_WIDTH / 2}px)`
+            }}
+            onPointerDown={(e) => {
+              e.stopPropagation()
+              // Add resize handler
+            }}
+          />
         </>
       )}
     </>
