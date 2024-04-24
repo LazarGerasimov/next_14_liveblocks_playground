9bd2e9058382dda3a318c9c9f8221840591e88d9
Laz <lazgerasimov@gmail.com>
2024-04-24 16:55:22 +0300

    add middle left resizing box on clicked rectangles

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -153,6 +153,23 @@ const SelectionBox = memo(({
               // Add resize handler
             }}
           />
+          <rect
+            className="fill-white stroke-1 stroke-blue-500"
+            x={0}
+            y={0}
+            style={{
+              cursor: "ew-resize",
+              width: `${HANDLE_WIDTH}px`,
+              height: `${HANDLE_WIDTH}px`,
+              transform: `translate(
+                ${bounds.x - HANDLE_WIDTH / 2}px, 
+                ${bounds.y - HANDLE_WIDTH / 2 + bounds.height / 2}px)`
+            }}
+            onPointerDown={(e) => {
+              e.stopPropagation()
+              // Add resize handler
+            }}
+          />
         </>
       )}
     </>
