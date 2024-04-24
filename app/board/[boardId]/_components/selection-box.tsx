1397f6fd9e688c60bc735248c3f007c2f7e09365
Laz <lazgerasimov@gmail.com>
2024-04-24 16:44:02 +0300

    add bottom left resizing box on clicked rectangles

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -136,6 +136,23 @@ const SelectionBox = memo(({
               // Add resize handler
             }}
           />
+          <rect
+            className="fill-white stroke-1 stroke-blue-500"
+            x={0}
+            y={0}
+            style={{
+              cursor: "nesw-resize",
+              width: `${HANDLE_WIDTH}px`,
+              height: `${HANDLE_WIDTH}px`,
+              transform: `translate(
+                ${bounds.x - HANDLE_WIDTH / 2}px, 
+                ${bounds.y - HANDLE_WIDTH / 2 + bounds.height}px)`
+            }}
+            onPointerDown={(e) => {
+              e.stopPropagation()
+              // Add resize handler
+            }}
+          />
         </>
       )}
     </>
