f65d02ffba8b557717e6cf56d456c17a19e71188
Laz <lazgerasimov@gmail.com>
2024-04-27 19:54:57 +0300

    add translating layers functionality (move shapes around the screen)

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -71,6 +71,35 @@ const Canvas = ({
     setCanvasState({ mode: CanvasMode.None });
   }, [lastUsedColor]);
 
+  const translateSelectedLayers = useMutation((
+    { storage, self },
+    point: Point,
+  ) => {
+    if (canvasState.mode !== CanvasMode.Translating) {
+      return;
+    }
+
+    const offset = {
+      x: point.x - canvasState.current.x,
+      y: point.y - canvasState.current.y
+    };
+
+    const liveLayers = storage.get("layers");
+
+    for (const id of self.presence.selection) {
+      const layer = liveLayers.get(id);
+
+      if (layer) {
+        layer.update({
+          x: layer.get("x") + offset.x,
+          y: layer.get("y") + offset.y
+        })
+      }
+    }
+
+    setCanvasState({ mode: CanvasMode.Translating, current: point });
+  }, [canvasState]);
+
   const resizeSelectedLayer = useMutation((
     { storage, self },
     point: Point
@@ -120,15 +149,20 @@ const Canvas = ({
 
     const current = pointerEventToCanvasPoint(e, camera);
 
-    if (canvasState.mode === CanvasMode.Resizing) {
+    if (canvasState.mode === CanvasMode.Translating) {
+      translateSelectedLayers(current);
+    } else if (canvasState.mode === CanvasMode.Resizing) {
       resizeSelectedLayer(current)
     }
+
+
 
     setMyPresence({ cursor: current })
   }, [
     camera,
     canvasState,
-    resizeSelectedLayer
+    resizeSelectedLayer,
+    translateSelectedLayers
   ]);
 
   const onPointerLeave = useMutation((
