e75a0d6c7bf400ee3c070604b12aa729d6e987d7
Laz <lazgerasimov@gmail.com>
2024-04-28 15:46:22 +0300

    add selection bounds to selection tools comp

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -1,19 +1,34 @@
 "use client";
 
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
+import { useSelf } from "@/liveblocks.config";
 import { Camera, Color } from "@/types/canvas";
+import { memo } from "react";
 
 interface SelectionToolsProps {
   camera: Camera;
   setLastUsedColor: (color: Color) => void;
 }
 
-const SelectionTools = ({
+const SelectionTools = memo(({
   camera,
   setLastUsedColor
 }: SelectionToolsProps) => {
+
+  const selection = useSelf((me) => me.presence.selection);
+
+  const selectionBounds = useSelectionBounds();
+
+  if (!selectionBounds) {
+    return null;
+  }
+
+  const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
+  const y = selectionBounds.y + camera.y;
+
   return (
     <div>SelectionTools</div>
   )
-}
+})
 
 export default SelectionTools
