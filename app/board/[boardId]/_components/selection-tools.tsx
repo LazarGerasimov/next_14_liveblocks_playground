cf2e5575ae4462b496a875f43e095cc10362ce80
Laz <lazgerasimov@gmail.com>
2024-04-30 20:02:09 +0300

    implement change colour functionality

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSelectionBounds } from "@/hooks/use-selection-bounds";
-import { useSelf } from "@/liveblocks.config";
+import { useMutation, useSelf } from "@/liveblocks.config";
 import { Camera, Color } from "@/types/canvas";
 import { memo } from "react";
 import ColorPicker from "./color-picker";
@@ -17,6 +17,18 @@ const SelectionTools = memo(({
 }: SelectionToolsProps) => {
 
   const selection = useSelf((me) => me.presence.selection);
+
+  const setFill = useMutation((
+    { storage },
+    fill: Color
+  ) => {
+    const liveLayers = storage.get("layers");
+    setLastUsedColor(fill);
+
+    selection.forEach((id) => {
+      liveLayers.get(id)?.set("fill", fill);
+    })
+  }, [selection, setLastUsedColor]);
 
   const selectionBounds = useSelectionBounds();
 
@@ -38,7 +50,7 @@ const SelectionTools = memo(({
       }}
     >
       <ColorPicker
-        onChange={() => { }}
+        onChange={setFill}
       />
     </div>
   )
