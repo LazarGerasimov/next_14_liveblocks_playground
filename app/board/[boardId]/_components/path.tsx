52d112b547acddcf464149105623ab5d82c72c36
Laz <lazgerasimov@gmail.com>
2024-05-10 19:08:48 +0300

    implement get svg path from stroke util function to path comp

--- app/board/[boardId]/_components/path.tsx
+++ app/board/[boardId]/_components/path.tsx
@@ -1,5 +1,6 @@
 
 
+import { getSvgPathFromStroke } from '@/lib/utils';
 import getStroke from 'perfect-freehand';
 
 interface PathProps {
@@ -22,7 +23,26 @@ const Path = ({
   stroke
 }: PathProps) => {
   return (
-    <path />
+    <path
+      className='drop-shadow-md'
+      onPointerDown={onPointerDown}
+      d={getSvgPathFromStroke(
+        getStroke(points, {
+          size: 16,
+          thinning: 0.5,
+          smoothing: 0.5,
+          streamline: 0.5
+        })
+      )}
+      style={{
+        transform: `translate(${x}px, ${y}px)`,
+      }}
+      x={0}
+      y={0}
+      fill={fill}
+      stroke={stroke}
+      strokeWidth={1}
+    />
   )
 }
 
